0463b619ac979f71a46a62eaeaf7a81ab7b030e0
Kevin Isom <kevinisom@Shenandoah.local>
2016-12-31 09:56:34 +1300

    verify last date is the expected date

--- test/index.js
+++ test/index.js
@@ -4,5 +4,10 @@ var tap = require('tap');
 var days = require('../index');
 var dateToTest = new Date('01/01/2017');
 
-var result = days(dateToTest, 45)
+var result = days(dateToTest, 45);
+
+var endDate = new Date('2017-02-14T00:00:00.000Z');
+var endFromList = new Date(result[44]);
+
 tap.equal(result.length, 45, 'check if the range is 45');
+tap.equal(endDate.valueOf(), endFromList.valueOf(), 'check if the last date is the expected date');
